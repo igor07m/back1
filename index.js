a5f4d05ac48e22033202671edd9a0cef3b89c87b
igor <igorabeloaz@gmail.com>
2024-03-12 14:57:42 -0300

    abrir arquivos usando fs

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+import fs from "fs";
+
 import chalk from 'chalk';
 
-console.log(chalk.blue('Olá mundo'));
 
-console.log(chalk.blue.bgWhite.bold("SENAI"));
 
-console.log(chalk.blue('Curso',' de',' Node.JS'));
+//console.log(chalk.blue('Olá mundo'));
 
-console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
+//console.log(chalk.blue.bgWhite.bold("SENAI"));
 
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+//console.log(chalk.blue('Curso',' de',' Node.JS'));
+
+//console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
+//console.log(`
+//CPU: ${chalk.red('90%')}
+//RAM: ${chalk.green('40%')}
+//DISK: ${chalk.yellow('70%')}
+//`);
+//2}tratamento de erros,function para tratar os erros
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code,"não há arquivo no diretorio"));
+}
+
+
+
+
+
+
+//1} escrever uma function que traga arquivos .md
+function pegaArquivo(caminhoDoArquivo){
+    const encoding="utf-8";
+    fs.readFile(caminhoDoArquivo,encoding,(erro,texto)=>{
+        if(erro){
+            trataErro(erro)
+        }
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
+//callback(erro,texto)
